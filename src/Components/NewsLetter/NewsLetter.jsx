e8afb12b83e454cabb9f3625f0df13452b62b6d8
Chamidini <swethachamidini@gmail.com>
2026-04-04 23:56:07 +0530

    add new <div>

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -6,7 +6,10 @@ const NewCollections = () => {
         <div className='newsletter'>
             <h1>Get Exclusive Offers On Your Email</h1>
             <p>Subscribe to our newletter and stay updated</p>
-            
+            <div>
+                <input type="email" placeholder="Your Email id" />
+                <button>Subscribe</button>
+            </div>
         </div>
     );
 }
