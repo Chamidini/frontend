ec945de44ef56f8fa3857bbbb88e3357b670a479
Chamidini <swethachamidini@gmail.com>
2026-04-04 23:54:22 +0530

    add h1 and p for 'newsletter' class

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -4,7 +4,9 @@ import './NewCollections.css';
 const NewCollections = () => {
     return (
         <div className='newsletter'>
-
+            <h1>Get Exclusive Offers On Your Email</h1>
+            <p>Subscribe to our newletter and stay updated</p>
+            
         </div>
     );
 }
