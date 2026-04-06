2cad0f47f85cf907961b738306eeba6ee6e16d1d
Chamidini <swethachamidini@gmail.com>
2026-04-06 19:45:37 +0530

    create 'ShopContext.jsx'

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,9 @@
+import React, { createContext } from 'react';
+import all_products from '../Components/Assets/all_products';
+
+export const ShopContext = createContext(null);
+
+const ShopContextProvider = (props) => {
+
+    const contexValue = {}
+}
