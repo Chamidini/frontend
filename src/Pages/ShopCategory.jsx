fd6ad36c233645612d19c0dbb1d5796d5d15b1a0
Chamidini <swethachamidini@gmail.com>
2026-04-08 00:51:14 +0530

    add '.jsx' for 'Item'

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -2,10 +2,10 @@ import React, { useContext } from "react";
 import './CSS/ShopCategory.css';
 import { ShopContext } from "../Context/ShopContext";
 import dropdown_icon from '../Components/Assets/dropdown_icon.png'
-import Item from '../Components/Item/Item'
+import Item from '../Components/Item/Item.jsx'
 
 const ShopCategory = (props) => {
-    const {all_products} = useContext(ShopContext);
+    const {all_product} = useContext(ShopContext);
     return (
         <div className="shop-category">
             <img src={props.banner} alt="" />
@@ -18,7 +18,7 @@ const ShopCategory = (props) => {
                 </div>
             </div>
             <div className="shopcategory-products">
-                {all_products.map((item,i)=>{
+                {all_product.map((item,i)=>{
                     if (props.category===item.category) {
                         return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
                     }
