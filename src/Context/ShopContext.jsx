fd401778a77842856ae45d0ccdc381df5f467907
Chamidini <swethachamidini@gmail.com>
2026-04-08 19:59:30 +0530

    replace 'all_products '

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,11 +1,11 @@
 import React, { createContext } from 'react';
-import all_product from '../Components/Assets/all_product.js';
+import all_products from '../Components/Assets/all_products';
 
 export const ShopContext = createContext(null);
 
 const ShopContextProvider = (props) => {
 
-    const contexValue = {all_product};
+    const contexValue = {all_products};
     return (
         <ShopContext.Provider value={contexValue}>
             {props.children}
