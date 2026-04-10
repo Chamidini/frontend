5030619c703f525e16d193e20fcb8a89e175e5f6
Chamidini <swethachamidini@gmail.com>
2026-04-10 23:34:00 +0530

    remove 'men_product.map...'

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -28,14 +28,6 @@ const ShopCategory = (props) => {
                     }
                 })}
 
-                {men_product.map((item,i) => {
-                    if (props.category=="men") {
-                        return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-                    }
-                    else{
-                        return null;
-                    }
-                })}
             </div>
         </div>
     );
