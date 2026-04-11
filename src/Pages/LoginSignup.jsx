af80124bd13716787592f3e1457eef27401be260
Chamidini <swethachamidini@gmail.com>
2026-04-11 20:25:09 +0530

    add 'loginsignup'

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -2,7 +2,16 @@ import React from "react";
 
 const LoginSignup = () => {
     return (
-        <div>
+        <div className="loginsignup">
+            <div className="loginsignup-container">
+                <h1>Sign Up</h1>
+                <div className="loginsignup-fields">
+                    <input type="text" placeholder="Your Name"/>
+                    <input type="email" placeholder="Email Address"/>
+                    <input type="password" placeholder="Password"/>
+                </div>
+                
+            </div>
             
         </div>
     );
