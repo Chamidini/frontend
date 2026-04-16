c69029a8b596bf07f27613137ebb1bfa21cb8ce9
Chamidini <swethachamidini@gmail.com>
2026-04-16 19:13:43 +0530

    export default Breadcrum;

--- src/Components/Breadcrums/Breadcrums.jsx
+++ src/Components/Breadcrums/Breadcrums.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import './Breadcrum.css'
+import './Breadcrums.css'
 import arrow_icon from '../Assets/breadcrum_arrow.png'
 
-const Breadcrums = (props) => {
+const Breadcrum = (props) => {
     const {product} =props;
     return (
-        <div className="breadcrums">
+        <div className="breadcrum">
             HOME <img src={arrow_icon} alt="" />SHOP<img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name} 
-
         </div>
     )
 }
+
+export default Breadcrum;
