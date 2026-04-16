58fe7a736a28bf35e2ea318986fc527c596e70fc
Chamidini <swethachamidini@gmail.com>
2026-04-17 00:18:47 +0530

    add 'productdisplay-right'

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -17,10 +17,17 @@ const ProductDisplay = (props) => {
                 <div className="productdisplay-img">
                     <img className="productdisplay-main-img" src={product.image} alt="" />
                 </div>
-
             </div>
             <div className="productdisplay-right">
-
+                <h1>{product.name}</h1>
+                <div className="productdisplay-right-star">
+                    <img src={star_icon} alt="" />
+                    <img src={star_icon} alt="" />
+                    <img src={star_icon} alt="" />
+                    <img src={star_icon} alt="" />
+                    <img src={star_dull_icon} alt="" />
+                    <p>(122)</p>
+                </div>
             </div>
         </div>
     )
