c32b4ac901b3a8071ceb712779ad4e810875e0ef
Chamidini <swethachamidini@gmail.com>
2026-04-19 00:22:03 +0530

    create 'DescriptionBox' jsx and css files

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './DescriptionBox.css'
+
+const DescriptionBox = (props) => {
+    return(
+        <div className="descriptionbox">
+
+        </div>
+
+    )
+}
+
+export default DescriptionBox;
