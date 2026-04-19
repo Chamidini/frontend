b88d71bdd366a545422e0019cffb6246628349cf
Chamidini <swethachamidini@gmail.com>
2026-04-20 00:59:42 +0530

    change relatedproducts-items

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -8,7 +8,7 @@ const RelatedProducts = (props) => {
         <div className="relatedproducts">
             <h1>Related Products</h1>
             <hr />
-            <div className="relatedProducts">
+            <div className="relatedproducts-items">
                 {data_product.map((item,i)=>{
                     return<Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
                 })}
