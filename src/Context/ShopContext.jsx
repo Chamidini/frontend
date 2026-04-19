8a586ca74fc31559bd7b088a5dde5b3880a5dd67
Chamidini <swethachamidini@gmail.com>
2026-04-20 01:49:14 +0530

    add onClick for btn

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,11 +1,29 @@
 import React, { createContext } from 'react';
 import all_product from '../Components/Assets/all_product';
+import { useState } from 'react';
 
 export const ShopContext = createContext(null);
 
-const ShopContextProvider = (props) => {
+const getDefaultCart = () => {
+    let cart = {};
+    for(let index = 0; index < all_product.length+1; index++){
+        cart[index] = 0;
+    }
+    return cart;
+};
 
-    const contexValue = {all_product};
+const ShopContextProvider = (props) => {
+    const [cartItems,setCartItems] = useState(getDefaultCart());
+
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}));
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1}));
+    }
+
+    const contexValue = {all_product,cartItems,addToCart,removeFromCart};
     return (
         <ShopContext.Provider value={contexValue}>
             {props.children}
