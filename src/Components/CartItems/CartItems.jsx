553041c6da4edb7ae7aa5e8ec059552a7c265852
Chamidini <swethachamidini@gmail.com>
2026-04-21 00:26:52 +0530

    add 'cartitems-format' class

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './CartItems.css'
 import { ShopContext } from "../../Context/ShopContext";
+import remove_icon from '../Assets/cart_cross_icon.png';
 
 const CartItems = () => {
     const {all_products,cartItems,removeFromCart} = useContext(ShopContext);
@@ -14,7 +15,24 @@ const CartItems = () => {
                 <p>Total</p>
                 <p>Remove</p>
             </div>
+            <hr />
+            {all_products.map((e) => {
+                if(cartItems[e.id]>0)
+                    {
+                        return <div>
+                                    <div className="cartitems-format">
+                                        <img src={e.image} alt="" className="carticon-product-icon" />
+                                        <p>{e.name}</p>
+                                        <p>${e.new_price}</p>
+                                        <button className="cartitems-qualitity">{cartItems[e.id]}</button>
+                                        <p>${e.new_price * cartItems[e.id]}</p>
+                                        <img src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
+                                    </div>
+                                    <hr />
+                                </div>
+                    }
 
+            })}
         </div>
 
     )
