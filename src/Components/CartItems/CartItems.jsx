71e6d5a1a3fd2e5e92c2c3e24c0d045be9dfda89
Chamidini <swethachamidini@gmail.com>
2026-04-20 23:59:40 +0530

    add 'cartitems-format-main'

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import './CartItems.css'
+import { ShopContext } from "../../Context/ShopContext";
 
-const CartItems = (props) => {
+const CartItems = () => {
+    const {all_products,cartItems,removeFromCart} = useContext(ShopContext);
     return (
         <div className="cartitems">
+            <div className="cartitems-format-main">
+                <p>Products</p>
+                <p>Title</p>
+                <p>Price</p>
+                <p>Quantity</p>
+                <p>Total</p>
+                <p>Remove</p>
+            </div>
 
         </div>
 
