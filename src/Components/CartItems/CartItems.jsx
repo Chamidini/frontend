85c9901d45a31a8d852f116327d018a724e6941f
Chamidini <swethachamidini@gmail.com>
2026-04-20 23:10:00 +0530

    creates 'CartItems.jsx' file

---
+++ src/Components/CartItems/CartItems.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './CartItems.css'
+
+const CartItems = (props) => {
+    return (
+        <div className="cartitems">
+
+        </div>
+
+    )
+}
+
+export default CartItems;
